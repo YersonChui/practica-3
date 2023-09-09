1ba63f1e8611c43d69900c81e55d095cfebb7a5c
YersonChui <142272272+YersonChui@users.noreply.github.com>
2023-09-08 22:38:24 -0400

    Update ejercicio 03_02.cpp

--- ejercicio 03_02.cpp
+++ ejercicio 03_02.cpp
@@ -1,4 +1,42 @@
-// Fecha creación: 06/09/2023
-// Fecha modificación: 07/09/2023
-// Número de ejericio: 2
-// Problema planteado: Elabore un programa que lea una matriz de orden n x n y la cambie la fila 1 porla fina n.
+// Fecha creaciÃ³n: 06/09/2023
+// Fecha modificaciÃ³n: 08/09/2023
+// NÃºmero de ejericio: 2
+// Problema planteado: espiral
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Ingrese el orden de la matriz espiral (n x n): ";cin >> n;
+    int matriz[20][20];
+    int valor = 1;  // Valor inicial para llenar la matriz
+    int minFila = 0, maxFila = n - 1;  // LÃ­mites de filas
+    int minColumna = 0, maxColumna = n - 1;  // LÃ­mites de columnas
+
+    while (valor <= n * n) {
+        for (int i = minColumna; i <= maxColumna; ++i) {
+            matriz[minFila][i] = valor++;
+        }
+        minFila++;
+        for (int i = minFila; i <= maxFila; ++i) {
+            matriz[i][maxColumna] = valor++;
+        }
+        maxColumna--;
+        for (int i = maxColumna; i >= minColumna; --i) {
+            matriz[maxFila][i] = valor++;
+        }
+        maxFila--;
+        for (int i = maxFila; i >= minFila; --i) {
+            matriz[i][minColumna] = valor++;
+        }
+        minColumna++;
+    }
+    cout << "Matriz espiral generada:" << endl;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << matriz[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
